89f89582dfa9e63364c2000ec5c2c97a53bde67d
ramimmojumdar <expertramim@gmial.com>
2024-05-05 14:55:27 +0600

    first commit

---
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.texttospeech;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech textToSpeech;
+    EditText editText;
+
+    AppCompatButton button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+            }
+        });
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        String myString = editText.getText().toString();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(myString,TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+
+
+    }
+}
